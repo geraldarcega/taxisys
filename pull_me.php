4d48f24eccabfdb639ae8510519a3df53cbd1254
gerald arcega <michaelgeraldarcega@gmail.com>
2015-08-26 16:12:42 +0800

    add pull_me

---
+++ pull_me.php
@@ -0,0 +1,41 @@
+<?php
+$continue = TRUE;
+$working_dir = realpath('../');
+	// The commands
+	$commands = array(
+		'echo $PWD',
+		'whoami',
+		'git pull',
+		'git status',
+	);
+?>
+<?php if($continue): ?>
+<?php
+	// Run the commands for output
+	$output = '';
+	foreach($commands AS $command){
+		// Run it
+		$tmp = shell_exec($command.' 2>&1');
+		// Output
+		$output .= "<span style=\"color: #6BE234;\">\$</span> <span style=\"color: #729FCF;\">{$command}\n</span>";
+		$output .= htmlentities(trim($tmp)) . "\n";
+	}
+ 
+	// Make it pretty for manual user access (and why not?)
+?>
+<!DOCTYPE HTML>
+<html lang="en-US">
+<head>
+	<meta charset="UTF-8">
+	<title>GIT DEPLOYMENT SCRIPT</title>
+</head>
+<body style="background-color: #000000; color: #FFFFFF; font-weight: bold; padding: 0 10px;">
+<pre>
+
+- Git Deployment Script v1 -<br/>
+ 
+<?php echo $output; ?>
+</pre>
+</body>
+</html>
+<?php endif; ?>
